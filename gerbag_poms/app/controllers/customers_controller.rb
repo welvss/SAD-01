4479ffdc355f108ec086c71585377d923cc9720d
Jgabagacina <jgabagacina@gmail.com>
2016-01-16 00:27:50 +0800

    Working Delete Button Customer

--- gerbag_poms/app/controllers/customers_controller.rb
+++ gerbag_poms/app/controllers/customers_controller.rb
@@ -1,22 +1,56 @@
 class CustomersController < ApplicationController
+	before_action :get_customer, only: [:edit, :update]
+	def get_customer
+		@customer = Customer.find(params[:CustomerID])
+		
+	end
+
+
 	def show
-		@customer = Customer.find(2)
+		
 		respond_to do |format|
 			format.html
 		end
 	end
+
+
 	def new
 		@customer = Customer.new
 	end
+
+
 	def index
+		
 		@customer = Customer.new(customer_params)
 		if @customer.save
 		redirect_to customers_path
+		end
+		
 	end
 
-	end
 	def create
+		redirect_to customers_path
 	end
+	def update
+		if @customer.update(customer_params)
+			 redirect_to customers_path
+			end
+	end
+
+
+	def edit
+		
+	end
+
+
+	def destroy
+		@customer = Customer.find(params[:id])
+		@customer.destroy
+		redirect_to customers_path
+	end
+
+
+
 
 	private
 	def customer_params
